cba1d209be06a4a4d5624821c5354794f5fbde0e
negroweed <134845563+negroweed@users.noreply.github.com>
2024-11-23 16:32:31 +0800

    Update twrp_gold.mk
    already defined in device.mk

--- twrp_gold.mk
+++ twrp_gold.mk
@@ -14,21 +14,6 @@
 # limitations under the License.
 #
 
-# Configure base.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Configure core_64_bit.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Configure launch_with_vendor_ramdisk.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-
-# Configure emulated_storage.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Configure twrp config common.mk
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Device specific configs
 $(call inherit-product, device/xiaomi/gold/device.mk)
 
@@ -38,6 +23,3 @@ PRODUCT_NAME := twrp_gold
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 2312DRAABG
 PRODUCT_MANUFACTURER := Xiaomi
-
-# Hide Reflash TWRP
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
